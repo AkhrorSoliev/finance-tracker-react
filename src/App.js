6818b00b8658513904cd1fd5805b91e011e87760
teacherakhror <akhrorteacher@gmail.com>
2022-05-22 23:09:37 +0500

    auth is ready added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext'
 
 // pages component
 import Navbar from './components/Navbar'
@@ -7,16 +8,19 @@ import Login from './pages/login/Login'
 import Signup from './pages/signup/Signup'
 
 function App() {
+  const { authIsReady } = useAuthContext()
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </Router>
+      {authIsReady && (
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </Router>
+      )}
     </div>
   )
 }
